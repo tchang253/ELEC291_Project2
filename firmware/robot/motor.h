f8b112ca55771b5cdb3967bd8535c4ca32dbade3
Akash Karthikeyan <karaka7514@gmail.com>
2026-03-27 16:30:35 -0700

    motor: slowed down PWM frequency to prevent shoot-through, and added full automated hardware testing program

--- firmware/robot/motor.h
+++ firmware/robot/motor.h
@@ -2,13 +2,12 @@
 #define MOTOR_H
 
 // Motor PWM control functions using TIM2.
-// Adapted from TimerPWM example by Jesus Calvino-Fraga.
+// Adapted from TimerPWM example
 // See motor.c for pin assignments.
 
 void Motor_Init(void);
 void Motor_Left(int speed);   // -100 to 100
 void Motor_Right(int speed);  // -100 to 100
-void Motor_Stop(void);        // coast (both motors off)
-void Motor_Brake(void);       // electronic brake (lock wheels)
+void Motor_Stop(void);        // electronic brake (both motors 0 duty cycle)
 
 #endif
